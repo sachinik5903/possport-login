3151c88043f1ae12ff4b51352997fcd4430ce0d0
sachinik5903 <79072139+sachinik5903@users.noreply.github.com>
2021-02-15 17:20:11 +0530

    Add files via upload

---
+++ passport-config.js
@@ -0,0 +1,29 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+
+
+
+function initialize(passport, getUserByemail, getUserById) {
+    const authenticateUser = async(email,password,done) => {
+        const user = getUserByemail(email)
+        if (user == null) {
+            return done(null, false , { message : 'No user with that Email'});
+        }
+        try{
+            if (await bcrypt.compare(password, user.password)) {
+                return done(null, user)
+            } else {
+                return done(null, false ,{ message: 'password is incorrect' });
+            }
+        } catch(e){
+            return done(e);              
+        }
+    }
+    passport.use(new LocalStrategy({usernameField : 'email'}, authenticateUser));
+    passport.serializeUser((user, done) => done(null,user.id))
+    passport.deserializeUser((id, done) => {
+        return done(null, getUserById(id))
+    })
+}
+
+module.exports = initialize;
